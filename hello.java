2b47bf17f792aeb1108616e0059e21cc4efc5cd3
libertain <1912921006@qq.com>
2017-11-21 14:50:37 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args){
-        System.out.println("Hello Lee,change happend ,auto gogogogo!");
+        System.out.println("Hello Lee,change happend ,auto gogogogo,I change it on the other computer!");
     }
 
 }
